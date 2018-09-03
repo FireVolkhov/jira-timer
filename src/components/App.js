e5ec86a2c7db0fae5ac4fced808e8037a73a524b
FireVolkhov <firevolkhov@gmail.com>
2018-09-03 20:04:28 +0300

    1.2.0

--- src/components/App.js
+++ src/components/App.js
@@ -6,218 +6,218 @@ import ListItem from "./ListItem";
 import SavePopup from "./SavePopup";
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = App.getInitialState();
-    }
+	constructor(props) {
+		super(props);
+		this.state = App.getInitialState();
+	}
 
-    static getInitialState(){
-        return {
-            open: false,
-            yes: false,
-            value: localStorage.getItem('jira-timer-log')
-        };
-    }
+	static getInitialState() {
+		return {
+			open: false,
+			yes: false,
+			value: localStorage.getItem('jira-timer-log')
+		};
+	}
 
-    componentDidMount() {
-        this._localStoreSet = _.debounce((value) => {
-            value  = this.preParse(value);
-            this.setState({value});
-            localStorage.setItem('jira-timer-log', value);
-        }, 300);
-    }
+	componentDidMount() {
+		this._localStoreSet = _.debounce((value) => {
+			value = this.preParse(value);
+			this.setState({value});
+			localStorage.setItem('jira-timer-log', value);
+		}, 300);
+	}
 
-    setValue(value) {
-        this._localStoreSet(value);
-        this.setState({
-            value: value,
-            yes: false
-        });
-    }
+	setValue(value) {
+		this._localStoreSet(value);
+		this.setState({
+			value: value,
+			yes: false
+		});
+	}
 
-    preParse(value) {
-        let lines = value.split('\n');
+	preParse(value) {
+		let lines = value.split('\n');
 
-        _.each(lines, (l, i) => {
-            let taskNameReg = /^(task page|ефыл зфпу)$/i;
-            let taskReg = /^\/browse\/([^?]+)(?:\?.*)?/i;
-            let now = /^(now|тщц)$/i;
+		_.each(lines, (l, i) => {
+			let taskNameReg = /^(task page|ефыл зфпу)$/i;
+			let taskReg = /^\/browse\/([^?]+)(?:\?.*)?/i;
+			let now = /^(now|тщц)$/i;
 
 
-            if (taskNameReg.test(l) && taskReg.test(location.pathname)) {
-                lines[i] = `task ${location.pathname.match(taskReg)[1]}`;
-            }
+			if (taskNameReg.test(l) && taskReg.test(location.pathname)) {
+				lines[i] = `task ${location.pathname.match(taskReg)[1]}`;
+			}
 
-            if (now.test(l)) {
-                let date = new Date();
-                let hours = date.getHours();
-                let minets = date.getMinutes();
+			if (now.test(l)) {
+				let date = new Date();
+				let hours = date.getHours();
+				let minets = date.getMinutes();
 
-                lines[i] = `${hours} ${minets > 9 ? minets : ('0' + minets)}`;
-            }
-        });
+				lines[i] = `${hours} ${minets > 9 ? minets : ('0' + minets)}`;
+			}
+		});
 
-        return lines.join('\n');
-    }
+		return lines.join('\n');
+	}
 
-    parseLog(value) {
-        let lines = value.split('\n');
-        lines = _.map(lines, (l) => l.trim());
-        _.remove(lines, (l) => !l);
+	parseLog(value) {
+		let lines = value.split('\n');
+		lines = _.map(lines, (l) => l.trim());
+		_.remove(lines, (l) => !l);
 
-        let blocks = [];
-        let currentBlock = {};
-        let init = false;
+		let blocks = [];
+		let currentBlock = {};
+		let init = false;
 
-        _.each(lines, (l) => {
-            let timeLine = /^((\d?\d)(?:\/|\s)(\d?\d)\s)?(\d?\d)(?:\s|:)(\d?\d)$/i;
+		_.each(lines, (l) => {
+			let timeLine = /^((\d?\d)(?:\/|\s)(\d?\d)\s)?(\d?\d)(?:\s|:)(\d?\d)$/i;
 
-            if (timeLine.test(l)) {
-                if (init) {
-                    currentBlock.stop = l;
-                    blocks.push(currentBlock);
-                }
-                init = true;
-                currentBlock = {
-                    id: _.uniqueId(),
-                    lines: [],
-                    start: l,
-                    stop: '',
-                    time: '',
-                    task: '',
-                    text: ''
-                };
+			if (timeLine.test(l)) {
+				if (init) {
+					currentBlock.stop = l;
+					blocks.push(currentBlock);
+				}
+				init = true;
+				currentBlock = {
+					id: _.uniqueId(),
+					lines: [],
+					start: l,
+					stop: '',
+					time: '',
+					task: '',
+					text: ''
+				};
 
-            } else {
-                currentBlock.lines.push(l);
-            }
-        });
+			} else {
+				currentBlock.lines.push(l);
+			}
+		});
 
-        _.each(blocks, (b) => {
-            _.each(b.lines, (l) => {
-               let taskLine = /^Task(?:\s)+(.+)$/i;
+		_.each(blocks, (b) => {
+			_.each(b.lines, (l) => {
+				let taskLine = /^Task(?:\s)+(.+)$/i;
 
-               if (!b.task && taskLine.test(l)) {
-                   b.task = l.match(taskLine)[1];
-               } else {
-                   b.text += l + '\n';
-               }
-            });
+				if (!b.task && taskLine.test(l)) {
+					b.task = l.match(taskLine)[1];
+				} else {
+					b.text += l + '\n';
+				}
+			});
 
-            let start = b.start.split(/(\s|:|\/)/);
-            let stop = b.stop.split(/(\s|:|\/)/);
+			let start = b.start.split(/(\s|:|\/)/);
+			let stop = b.stop.split(/(\s|:|\/)/);
 
-            start = _.filter(start, (x) => x !== ' ' && x !== '/' && x !== ':');
-            stop = _.filter(stop, (x) => x !== ' ' && x !== '/' && x !== ':');
+			start = _.filter(start, (x) => x !== ' ' && x !== '/' && x !== ':');
+			stop = _.filter(stop, (x) => x !== ' ' && x !== '/' && x !== ':');
 
-            if (start.length === 4) {
-                let date = new Date();
+			if (start.length === 4) {
+				let date = new Date();
 
-                date.setMilliseconds(0);
-                date.setSeconds(0);
-                date.setMinutes(0);
-                date.setHours(0);
-                date.setDate(0);
-                date.setMonth(0);
+				date.setMilliseconds(0);
+				date.setSeconds(0);
+				date.setMinutes(0);
+				date.setHours(0);
+				date.setDate(0);
+				date.setMonth(0);
 
-                date.setDate(start[0]);
-                date.setMonth(start[1] - 1);
+				date.setDate(start[0]);
+				date.setMonth(start[1] - 1);
 
-                b.date = date;
+				b.date = date;
 
-                start.splice(0, 2);
-            }
+				start.splice(0, 2);
+			}
 
-            if (stop.length === 4) {
-                stop.splice(0, 2);
-            }
+			if (stop.length === 4) {
+				stop.splice(0, 2);
+			}
 
-            b.start = parseInt(start[0]) * 60 + parseInt(start[start.length - 1]);
-            b.stop = parseInt(stop[0]) * 60 + parseInt(stop[stop.length - 1]);
+			b.start = parseInt(start[0]) * 60 + parseInt(start[start.length - 1]);
+			b.stop = parseInt(stop[0]) * 60 + parseInt(stop[stop.length - 1]);
 
-            b.time = b.stop - b.start;
-        });
+			b.time = b.stop - b.start;
+		});
 
-        _.each(blocks, (b) => {
-            if (b.text.trim() && b.task) {
-                let clones = _.filter(blocks, (x) => x.id !== b.id && x.task === b.task);
-                let cleanClones = _.filter(clones, (x) => !x.text.trim());
-                let cleanIds = _.map(cleanClones, 'id');
+		_.each(blocks, (b) => {
+			if (b.text.trim() && b.task) {
+				let clones = _.filter(blocks, (x) => x.id !== b.id && x.task === b.task);
+				let cleanClones = _.filter(clones, (x) => !x.text.trim());
+				let cleanIds = _.map(cleanClones, 'id');
 
-                _.each(blocks, (x) => {
-                    if (~cleanIds.indexOf(x.id)) {
-                        x.needDelete = true;
-                        b.time += x.time;
-                        return true;
-                    }
-                });
-            }
-        });
+				_.each(blocks, (x) => {
+					if (~cleanIds.indexOf(x.id)) {
+						x.needDelete = true;
+						b.time += x.time;
+						return true;
+					}
+				});
+			}
+		});
 
-        _.remove(blocks, (b) => b.needDelete);
+		_.remove(blocks, (b) => b.needDelete);
 
-        blocks = _.map(blocks, (b) => {
-            let date = b.date || new Date();
+		blocks = _.map(blocks, (b) => {
+			const date = b.date || new Date();
 
-            date.setMilliseconds(0);
-            date.setSeconds(0);
-            date.setMinutes(0);
-            date.setHours(0);
+			date.setMilliseconds(0);
+			date.setSeconds(0);
+			date.setMinutes(0);
+			date.setHours(0);
 
-            date.setMinutes(b.stop);
+			date.setMinutes(b.stop);
 
-            date = moment(date);
-            date = date.format('DD/MMM/YY hh:mm A');
-            b.date = date;
+			b.rawDate = moment(date);
+			b.date = b.rawDate.format('DD/MMM/YY hh:mm A');
 
-            return b;
-        });
+			return b;
+		});
 
-        return this.refs.popup
-            .open(blocks)
-            .then(() => this.setValue(''))
-            .then(() => this.setState({yes: true}))
-            .catch((e) => console.error(e));
-    }
+		return this.refs.popup
+			.open(blocks)
+			.then(() => this.setValue(''))
+			.then(() => this.setState({yes: true}))
+			.catch((e) => console.error(e));
+	}
 
-    render() {
-        let cssClass = 'app app_jira-timer app_open_' +
-            (this.state.open ? 'yes' : 'no');
+	render() {
+		let cssClass = 'app app_jira-timer app_open_' +
+			(this.state.open ? 'yes' : 'no');
 
-        let manual = '09 50\n' +
-            'task Unit6-2\n' +
-            'Так выглядит обычный лог\n\n' +
-            '10 50\n' +
-            'Если нужно, можно сделать пустой блок без таски и он не залогируется\n\n' +
-            '11 00\n' +
-            'task page\n' +
-            'При написании названия таски "page" номер будет взят со странице\n\n' +
-            'now\n' +
-            '"now" заменяется на текущее время\n\n' +
-            '21/12 12 10\n' +
-            'Так можно указать конкретную дату для времени\n' +
-            'После нажатия "save" покажеться попап с данными которые будут залогированы\n\n' +
-            '21/12 13 40';
+		let manual = '09 50\n' +
+			'task Unit6-2\n' +
+			'Так выглядит обычный лог\n\n' +
+			'10 50\n' +
+			'Если нужно, можно сделать пустой блок без таски и он не залогируется\n\n' +
+			'11 00\n' +
+			'task page\n' +
+			'При написании названия таски "page" номер будет взят со странице\n\n' +
+			'now\n' +
+			'"now" заменяется на текущее время\n\n' +
+			'21/12 12 10\n' +
+			'Так можно указать конкретную дату для времени\n' +
+			'После нажатия "save" покажеться попап с данными которые будут залогированы\n\n' +
+			'21/12 13 40';
 
-        return <div className={cssClass}>
+		return <div className={cssClass}>
             <textarea
-                className="text"
-                value={this.state.value}
-                onChange={(e) => this.setValue(e.target.value)}
-                onFocus={() => this.setState({value: localStorage.getItem('jira-timer-log')})}
-                placeholder={manual}/>
-            <button
-                className="save-button"
-                onClick={() => this.parseLog(this.state.value)}
-                style={{color: this.state.yes ? 'green' : null}}
-                >SAVE</button>
-            <SavePopup ref="popup"/>
-            <ListItem
-                className='hide-button'
-                openPanel={this.state.open}
-                onClick={() => this.setState({open: !this.state.open, value: localStorage.getItem('jira-timer-log')})}>
-                <div className='list-item__text'>{this.state.open ? 'Скрыть' : 'Показать'}</div>
-            </ListItem>
-        </div>;
-    }
+	            className="text"
+	            value={this.state.value}
+	            onChange={(e) => this.setValue(e.target.value)}
+	            onFocus={() => this.setState({value: localStorage.getItem('jira-timer-log')})}
+	            placeholder={manual}/>
+			<button
+				className="save-button"
+				onClick={() => this.parseLog(this.state.value)}
+				style={{color: this.state.yes ? 'green' : null}}
+			>SAVE
+			</button>
+			<SavePopup ref="popup"/>
+			<ListItem
+				className='hide-button'
+				openPanel={this.state.open}
+				onClick={() => this.setState({open: !this.state.open, value: localStorage.getItem('jira-timer-log')})}>
+				<div className='list-item__text'>{this.state.open ? 'Скрыть' : 'Показать'}</div>
+			</ListItem>
+		</div>;
+	}
 }
--- src/services/jira-saver.js
+++ src/services/jira-saver.js
@@ -1,42 +1,70 @@
 export default {
-    multiSave(timers) {
-        timers = _.filter(timers, (t) => t.task && t.time > 0 && t.date && t.text);
-        return Promise.all(_.map(timers, (t) => this.saveTime(t.task, t.time + 'm', t.date, t.text)));
-    },
+	multiSave(timers) {
+		timers = _.filter(timers, (t) => t.task && t.time > 0 && t.date && t.text);
+		return Promise.all(_.map(timers, (t) => this.saveTime(t.task, t.time + 'm', t.date, t.text)));
+	},
 
-    saveTime(task, time, date, comment) {
-        return new Promise((resolve, reject) => {
-            //$.ajax({url: `https://5evenagency.atlassian.net/browse/${task}`})
-            $.ajax({url: `/browse/${task}`})
-                .done((r) => {
-                    let matches = r.match(/data-issue-key=.+rel="(\d+)"/);
-                    let id = matches[1];
+	/**
+	 *
+	 * @param task - CORE-1705
+	 * @param time - minutes
+	 * @param date - moment
+	 * @param comment - string
+	 * @return {Promise<any>}
+	 */
+	saveTime(task, time, date, comment) {
+		return new Promise((resolve, reject) => {
+			const data = {
+				timeSpentSeconds: time * 60,
+				comment: `<p>${comment}</p>`,
+				started: date.toISOString()
+			};
 
-                    matches = r.match(/<meta\s+id="atlassian-token"\s+name="atlassian-token"\s+content="([^"]+)">/);
-                    let token = matches[1];
+			$.ajax({
+				type: 'POST',
+				//url: 'https://5evenagency.atlassian.net/rest/internal/2/issue/CORE-1705/worklog?adjustEstimate=auto',
+				url: `/rest/internal/2/issue/${task}/worklog?adjustEstimate=auto`,
+				// {
+				// "timeSpentSeconds":10800,
+				// "comment":"<p>Добавил учёт настройки для выставления в подозрительные события.</p>",
+				// "started":"2018-09-03T13:35:52.118+0500"
+				// }
+				data: JSON.stringify(data),
+			})
+				.done((r) => resolve(r))
+				.fail((e) => reject(e));
 
-                    let data = [
-                        'inline=true',
-                        'decorator=dialog',
-                        `id=${id}`,
-                        `timeLogged=${time}`,
-                        `startDate=${date}`,
-                        'adjustEstimate=auto',
-                        `comment=${comment}`,
-                        `atl_token=${token}`
-                    ];
+			//$.ajax({url: `https://5evenagency.atlassian.net/browse/${task}`})
+		// 	$.ajax({url: `/browse/${task}`})
+		// 		.done((r) => {
+		// 			let matches = r.match(/data-issue-key=.+rel="(\d+)"/);
+		// 			let id = matches[1];
+		//
+		// 			matches = r.match(/<meta\s+id="atlassian-token"\s+name="atlassian-token"\s+content="([^"]+)">/);
+		// 			let token = matches[1];
+		//
+		// 			let data = [
+		// 				'inline=true',
+		// 				'decorator=dialog',
+		// 				`id=${id}`,
+		// 				`timeLogged=${time}`,
+		// 				`startDate=${date}`,
+		// 				'adjustEstimate=auto',
+		// 				`comment=${comment}`,
+		// 				`atl_token=${token}`
+		// 			];
+		//
+		// 			$.ajax({
+		// 				type: 'POST',
+		// 				url: '/secure/CreateWorklog.jspa',
+		// 				//url: 'https://5evenagency.atlassian.net/secure/CreateWorklog.jspa',
+		// 				data: data.join('&')
+		// 			})
+		// 				.done((r) => resolve(r))
+		// 				.fail((e) => reject(e));
+		// 		})
+		// 		.fail((e) => reject(e));
+		});
 
-                    $.ajax({
-                        type: 'POST',
-                        url: '/secure/CreateWorklog.jspa',
-                        //url: 'https://5evenagency.atlassian.net/secure/CreateWorklog.jspa',
-                        data: data.join('&')
-                    })
-                        .done((r) => resolve(r))
-                        .fail((e) => reject(e));
-                })
-                .fail((e) => reject(e));
-        });
-
-    }
+	}
 }
